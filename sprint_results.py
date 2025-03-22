5fe072cce9cf5cb305ef333d0757cb154be71ef1
Ilya Skvortsov <mail.ilyascw@gmail.com>
2025-03-22 20:55:57 +0300

    Добавил запуск в 00:00

--- sprint_results.py
+++ sprint_results.py
@@ -170,7 +170,7 @@ async def scheduler(bot: Bot):
     while True:
         try:
             now = datetime.now()
-            target_time = now.replace(hour=19, minute=7, second=0, microsecond=0)
+            target_time = now.replace(hour=00, minute=00, second=0, microsecond=0)
             sleep_time = (target_time - now).total_seconds()
 
             if sleep_time < 0:
